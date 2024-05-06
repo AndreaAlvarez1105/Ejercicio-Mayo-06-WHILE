3f786cc4321077b3a2f7f8b04b74b5c431baf0a8
Lic. Daysi Lopez Reyez <00073824@uca.edu.sv>
2024-05-06 13:03:47 -0600

    Ejercicios clase

---
+++ 05_06_Ejercicio01.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n = 0;
+int i = 1;
+
+cout << "Ingrese un numero positivo: ";
+cin >> n;
+
+
+while (i<10)
+{
+    cout << i << '\n';
+    i = i + 1;
+
+    cout << i << '\n';
+    i = i + 1;
+
+    cout << i << '\n';
+    i=i+1;
+
+    cout << i << '\n';
+    i=i+1;
+
+    cout << i << '\n';
+    i = i +1;
+
+    cout << i << '\n';
+    i = i +1;
+
+    cout << i << '\n';
+    i = i +1;
+
+    cout << i << '\n';
+    i = i +1;
+
+    cout << i << '\n';
+    i = i +1;
+
+    
+}
+return 0;
+
+
+}
